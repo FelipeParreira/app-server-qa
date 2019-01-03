f6324b111a3c9200c0f65862e33bf9390b360cb5
Felipe Parreira <felipeparreirafb@gmail.com>
2019-01-03 13:32:57 -0800

    Add route for the testQA

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/testQA', (req, res) => {
+  res.send('Response from QA server!');
+});
+
 module.exports = router;
