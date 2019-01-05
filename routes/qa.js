ce5e1b740841ba40b2039e577582f0572b04de2d
Felipe Parreira <felipeparreirafb@gmail.com>
2019-01-05 06:54:23 -0800

    Add route to get all questions for a hotel

---
+++ routes/qa.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const axios = require('axios');
+const util = require('util');
+const router = express.Router();
+
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,POST');
+  next();
+});
+
+router.get('/hotels/:hotelId/questions', (req, res, next) => {
+  const { hotelId } = req.params;
+  axios.get(`http://node-express-env-service-qa.swpb5j5env.us-west-2.elasticbeanstalk.com/hotels/${hotelId}/questions`)
+  .then(response => {
+    res.send(response.data);
+  });
+});
