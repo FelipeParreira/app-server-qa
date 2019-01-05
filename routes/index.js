969865d6a7544b0b2de09a4b75cb1bc8af74889f
Felipe Parreira <felipeparreirafb@gmail.com>
2019-01-05 15:27:03 -0800

    dummy response in get questions

--- routes/index.js
+++ routes/index.js
@@ -37,6 +37,7 @@ router.get('/', function(req, res, next) {
 
 // GET questions for a certain hotel
 router.get('/hotels/:hotelId/questions', (req, res) => {
+  res.send(['alll']);
   const { hotelId } = req.params;
   getAllQuestions(hotelId, res);
 });
